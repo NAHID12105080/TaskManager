d701f1cb5416abf499559c90db69d821d1ec2a48
Nahid <nahid151341@gmail.com>
2024-03-17 21:26:00 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Full Stack TaskManager App
+
+Full Stack Todo List App built with Next.js 14 (Server actions) and supabase
+Its a very basic todo list app to get started with **Next.js** Server actions.
+
+## Technologies Used
+
+- Next js 14 (Server actions)
+- Typescript
+- Tailwind CSS
+- Shadcn-Ui
+- Prisma
+- Zod
+- supabase
+
+## Key Features
+
+- Create Todos
+- Mark todo completed
+- Delete Todo
+- Different sections for pending and completed tasks.
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Tapestry } from "next/font/google";
+import "./globals.css";
+import ToastProvider from "@/components/ui/Toaster";
+
+const font = Tapestry({ subsets: ["latin"], weight: "400" });
+
+export const metadata: Metadata = {
+  title: "Task Manager",
+  description:
+    "Be Productive by managing tasks efficiently. Every task is a step closer to your goal.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="png" href="logo.png" />
+      </head>
+      <body className={font.className}>
+        <ToastProvider />
+        {children}
+      </body>
+    </html>
+  );
+}
