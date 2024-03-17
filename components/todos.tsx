4aada740fa888b25ba27b9ebb739863dd224bb97
Nahid <nahid151341@gmail.com>
2024-03-17 21:38:20 +0600

    spelling correction

--- components/todos.tsx
+++ components/todos.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import TodoItem from "@/components/todoitem";
-import { Todo } from "@prisma/client";
+import { todo } from "@prisma/client";
 
 interface TodosProps {
-  todos: Todo[];
+  todos: todo[];
 }
 export default function Todos({ todos }: TodosProps) {
   return (
     <main className="my-8">
       {todos.length > 0 ? (
-        todos.map((todo: Todo) => {
+        todos.map((todo: todo) => {
           return <TodoItem key={todo.id} {...todo} />;
         })
       ) : (
